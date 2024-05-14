e57dadf0aae662c022b07b22cd20910b651d5334
NicoFani <nicolasfani87@gmail.com>
2024-05-13 23:46:47 -0300

    add the current date dynamically

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskComponent } from '../../components/task/task.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,21 @@ import { TaskComponent } from '../../components/task/task.component';
   imports: [TaskComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  providers: [DatePipe],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   tasks = [
     { name: 'Task 1', description: 'Make the bed', isDone: true },
     { name: 'Task 2', description: 'Wash the dishes', isDone: false },
     { name: 'Task 3', description: 'Go to supermarket', isDone: false },
   ];
+
+  fecha: any = '';
+
+  constructor(private datePipe: DatePipe) {}
+
+  ngOnInit() {
+    const today = new Date();
+    this.fecha = this.datePipe.transform(today, 'MMMM d, y');
+  }
 }
